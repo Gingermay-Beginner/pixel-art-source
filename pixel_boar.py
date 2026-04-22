675ecfc3b7445dbc0207f23e5dadc6fbcf3774c4
Ubuntu <azureuser@openclaw-vm.bra1bihndfwuhlzprzergw5lbe.cx.internal.cloudapp.net>
2026-04-22 15:57:32 +0000

    boar: x=7 face use BOAR_L not GROUND

--- pixel_boar.py
+++ pixel_boar.py
@@ -222,7 +222,7 @@ def draw_boar(ox, oy, facing='right'):
     lfl(4,10, 7,17, BOAR_L)
     for lx,ly in [(17,4),(17,10)]: lsp(lx,ly,GROUND)
     for _ly in range(4,11): lsp(17,_ly,GROUND)
-    lsp(7,4,GROUND); lsp(7,10,GROUND); lsp(7,5,GROUND)
+    lsp(7,4,BOAR_L); lsp(7,10,BOAR_L); lsp(7,5,BOAR_L)
     # 耳朵
     lfl(2,4, 8,10, BOAR_LT)
     lfl(2,4, 14,16, BOAR_LT)
