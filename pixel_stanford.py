b3334b211c6482d8352de3ff1ea60cf5420219ad
Ubuntu <azureuser@openclaw-vm.bra1bihndfwuhlzprzergw5lbe.cx.internal.cloudapp.net>
2026-04-27 17:20:42 +0000

    pixel_stanford: side roof 3-row with vertical tile ridges

--- pixel_stanford.py
+++ pixel_stanford.py
@@ -327,14 +327,18 @@ def sp2(x, y, c):
     img2.paste(px,(x*S2,y*S2))
 img2 = img.copy()
 for _wx in range(0, 16):
-    _rtop = 6 + round(2 * (15 - _wx) / 15)
+    _rtop = 7 + round(2 * (15 - _wx) / 15)
     for _wr in range(_rtop, 10):
         _c = _SRL if _wr == _rtop else (_SR if _wr == _rtop+1 else _SRD)
+        if _wx % 3 == 1: _c = _SRD
+        elif _wx % 3 == 2: _c = _SRL
         sp2(_wx, _wr, _c)
 for _wx in range(49, 64):
-    _rtop = 8 - round(2 * (63 - _wx) / 14)
+    _rtop = 9 - round(2 * (63 - _wx) / 14)
     for _wr in range(_rtop, 10):
         _c = _SRL if _wr == _rtop else (_SR if _wr == _rtop+1 else _SRD)
+        if _wx % 3 == 1: _c = _SRD
+        elif _wx % 3 == 2: _c = _SRL
         sp2(_wx, _wr, _c)
 img2.save('pixel_stanford.png')
 print('Saved')
