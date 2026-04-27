ababb797dc84ca8e430512eca90fd71e4f5fdb50
Ubuntu <azureuser@openclaw-vm.bra1bihndfwuhlzprzergw5lbe.cx.internal.cloudapp.net>
2026-04-27 19:28:30 +0000

    draft_fountain: tan color, right+1, water column 3wide

--- draft_fountain.py
+++ draft_fountain.py
@@ -24,25 +24,31 @@ def wcol(x, y1, y2, c):
     for y in range(y1, y2+1):
         sp(x, y, c)
 
-FONT_S = (178, 195, 212)
-FONT_D = (148, 162, 178)
+FONT_S = (212, 188, 148)
+FONT_D = (182, 158, 118)
 WATER   = ( 88, 148, 188)
 WATER_L = (138, 188, 218)
 SPRAY   = (178, 215, 238)
 FX = 32
 
-fl(20, 23, 22, 41, FONT_S)
-wrow(19, 24, 39, FONT_S)
-wrow(24, 24, 39, FONT_D)
+# 第一层水盆外缘 x=22~42（+1右）
+fl(20, 23, 22, 42, FONT_S)
+wrow(19, 24, 40, FONT_S)
+wrow(24, 24, 40, FONT_D)
 wcol(22, 20, 23, FONT_D)
-wcol(41, 20, 23, FONT_D)
-fl(20, 23, 23, 40, WATER)
-wrow(20, 25, 38, WATER_L)
-for _wx in [26, 29, 33, 37]:
+wcol(42, 20, 23, FONT_D)
+# 水面 x=23~41
+fl(20, 23, 23, 41, WATER)
+wrow(20, 25, 39, WATER_L)
+for _wx in [26, 29, 33, 38]:
     sp(_wx, 22, WATER_L)
+
+# 喷水柱（不变）
 for _wy in range(15, 20):
     sp(31, _wy, WATER)
     sp(32, _wy, WATER_L)
+    sp(33, _wy, WATER)
+# 水花（不变）
 for _wy in [15, 16]:
     _w = 16 - _wy
     for _wx in range(FX-_w, FX+_w+1):
@@ -51,21 +57,31 @@ for _wy in [15, 16]:
 sp(FX-2, 15, SPRAY); sp(FX+2, 15, SPRAY)
 sp(FX-1, 14, SPRAY); sp(FX, 14, SPRAY); sp(FX+1, 14, SPRAY)
 sp(FX, 13, SPRAY)
-fl(24, 25, 26, 37, FONT_D)
-wrow(24, 27, 36, FONT_S)
-fl(25, 28, 30, 33, FONT_D)
+
+# 底座 x=26~38
+fl(24, 25, 26, 38, FONT_D)
+wrow(24, 27, 37, FONT_S)
+
+# 连接柱 x=30~34
+fl(25, 28, 30, 34, FONT_D)
 wcol(30, 25, 28, FONT_S)
-wcol(33, 25, 28, FONT_S)
-fl(29, 30, 18, 45, FONT_S)
-wrow(28, 20, 43, FONT_S)
-wrow(31, 20, 43, FONT_D)
+wcol(34, 25, 28, FONT_S)
+
+# 第二层水盆 x=18~46
+fl(29, 30, 18, 46, FONT_S)
+wrow(28, 20, 44, FONT_S)
+wrow(31, 20, 44, FONT_D)
 wcol(18, 29, 30, FONT_D)
-wcol(45, 29, 30, FONT_D)
-fl(29, 30, 19, 44, WATER)
-wrow(29, 20, 43, WATER_L)
-for _wx in [22, 27, 32, 37, 42]:
+wcol(46, 29, 30, FONT_D)
+# 水面 x=19~45
+fl(29, 30, 19, 45, WATER)
+wrow(29, 20, 44, WATER_L)
+for _wx in [22, 27, 32, 38, 43]:
     sp(_wx, 30, WATER_L)
-fl(31, 32, 22, 41, FONT_D)
-wrow(31, 23, 40, FONT_S)
+
+# 第二层底座 x=22~42
+fl(31, 32, 22, 42, FONT_D)
+wrow(31, 23, 41, FONT_S)
 
 img.save('/tmp/fountain_draft.png')
+print('Saved')
