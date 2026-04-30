740274bf279d6bbe6b77904b447043d06e492f81
Ubuntu <azureuser@openclaw-vm.bra1bihndfwuhlzprzergw5lbe.cx.internal.cloudapp.net>
2026-04-30 18:15:33 +0000

    fabrini2: 左上伞位置调整

--- pixel_fabrini2.py
+++ pixel_fabrini2.py
@@ -447,7 +447,7 @@ img = _rgba2.convert('RGB')
 img.paste(_table_snap, (22*S, _TY1))
 
 # ── 伞（最上层）──
-draw_umbrella_top(-2, 24, 16, 'white')       # 左上伞
+draw_umbrella_top(0, 26, 16, 'white')       # 左上伞
 draw_umbrella_top(19, 42, 16, 'thin_stripe') # 左下伞（被同心圆盖住）
 draw_umbrella_top(62, 4, 16, 'ring')         # 右上伞
 draw_umbrella_top(46, 37, 16, 'ring')        # 右伞（同心环，最顶层）
